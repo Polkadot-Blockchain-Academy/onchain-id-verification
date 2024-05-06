2d8d3924bdfecfe765dc738e48cd7aa4c2371fd9
Nikos Kontakis <wirednkod@gmail.com>
2024-05-06 15:22:05 +0300

    Add consts

--- src/App.tsx
+++ src/App.tsx
@@ -1,83 +1,12 @@
 /* eslint-disable prefer-const */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { SetStateAction, useState } from 'react'
-import type { Binary } from "polkadot-api"
 import { Button, Input, Table } from 'antd'
 import { isValidAddress } from "@polkadot-ui/utils"
-
 const { TextArea } = Input
 
 import './App.css'
-import { DotQueries, dot } from '@polkadot-api/descriptors'
-import { polkadotClient } from './clients'
-
-const columns = [
-  {
-    title: 'Has ID',
-    dataIndex: 'hasId',
-    key: 'hasId',
-    render: (v: boolean) => v ? "✅" : "🚫",
-  },
-  {
-    title: 'Display Name',
-    dataIndex: 'display',
-    key: 'display',
-  },
-  {
-    title: 'Legal Name',
-    dataIndex: 'name',
-    key: 'name',
-  },
-  {
-    title: 'Github',
-    dataIndex: 'github',
-    key: 'github',
-  },
-  {
-    title: 'Email',
-    dataIndex: 'email',
-    key: 'email',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
-  },
-];
-
-const invColumns = [
-  {
-    title: 'Invalid',
-    dataIndex: 'address',
-    key: 'address',
-  },
-];
-
-const identityDataToString = (value: number | string | Binary | undefined) =>
-  typeof value === "object" ? value.asText() : value ?? ""
-
-const mapRawIdentity = (
-  rawIdentity?: DotQueries["Identity"]["IdentityOf"]["Value"]
-) => {
-  if (!rawIdentity) return rawIdentity
-  const {
-    info: { additional, display, legal, email },
-  } = rawIdentity[0]
-
-  const display_id = identityDataToString(display.value)
-  const legal_read = identityDataToString(legal.value)
-  const email_read = identityDataToString(email.value)
-  const additionalInfo = Object.fromEntries(
-    additional.map(([key, { value }]) => [
-      identityDataToString(key.value!),
-      identityDataToString(value),
-    ])
-  )
-
-  return { ...additionalInfo, display: display_id, name: legal_read, email: email_read }
-}
-
-const api = polkadotClient?.getTypedApi(dot)
+import { api, mapRawIdentity, columns, invColumns } from './consts'
 
 export const App = () => {
   const [members, setMembers] = useState<string[]>([])
---
+++ src/consts.ts
@@ -0,0 +1,72 @@
+import type { Binary } from "polkadot-api"
+import { DotQueries, dot } from '@polkadot-api/descriptors'
+import { polkadotClient } from './clients'
+
+export const columns = [
+    {
+        title: 'Has ID',
+        dataIndex: 'hasId',
+        key: 'hasId',
+        render: (v: boolean) => v ? "✅" : "🚫",
+    },
+    {
+        title: 'Display Name',
+        dataIndex: 'display',
+        key: 'display',
+    },
+    {
+        title: 'Legal Name',
+        dataIndex: 'name',
+        key: 'name',
+    },
+    {
+        title: 'Github',
+        dataIndex: 'github',
+        key: 'github',
+    },
+    {
+        title: 'Email',
+        dataIndex: 'email',
+        key: 'email',
+    },
+    {
+        title: 'Address',
+        dataIndex: 'address',
+        key: 'address',
+    },
+];
+
+export const invColumns = [
+    {
+        title: 'Invalid',
+        dataIndex: 'address',
+        key: 'address',
+    },
+];
+
+
+export const identityDataToString = (value: number | string | Binary | undefined) =>
+    typeof value === "object" ? value.asText() : value ?? ""
+
+export const mapRawIdentity = (
+    rawIdentity?: DotQueries["Identity"]["IdentityOf"]["Value"]
+) => {
+    if (!rawIdentity) return rawIdentity
+    const {
+        info: { additional, display, legal, email },
+    } = rawIdentity[0]
+
+    const display_id = identityDataToString(display.value)
+    const legal_read = identityDataToString(legal.value)
+    const email_read = identityDataToString(email.value)
+    const additionalInfo = Object.fromEntries(
+        additional.map(([key, { value }]) => [
+            identityDataToString(key.value!),
+            identityDataToString(value),
+        ])
+    )
+
+    return { ...additionalInfo, display: display_id, name: legal_read, email: email_read }
+}
+
+export const api = polkadotClient?.getTypedApi(dot)
